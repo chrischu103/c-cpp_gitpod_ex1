76e26329247427455e34ec4024ef20b23bfba862
chrischu103 <100347499+chrischu103@users.noreply.github.com>
2022-04-14 20:46:26 +0800

    Create ex17.cpp

---
+++ ex17.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   //int i[9] = {1,2,3,5,6,9,8,7,4};
+    int a[3] = {5,6,2};
+    int i,j;
+    
+    for(i=0;i < 2;i++){
+        for(j=0;j < 2;j++){
+            if(a[j] > a[j+1]){
+                int temp = a[j+1];
+                    a[j+1] = a[j];
+                    a[j] = temp;
+                
+            }
+        }
+    }
+    for(i=0;i<3;i++){
+        cout << "a[" << i << "]=" << a [i] << " " ;
+    }
+    cout << endl;
+    
+    cout<<"Hello World";
+
+    return 0;
+}
