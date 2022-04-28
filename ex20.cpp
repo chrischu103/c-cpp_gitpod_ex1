bc102a66c4d0dfcf805d8866e0ba8a45c4958b1d
chrischu103 <100347499+chrischu103@users.noreply.github.com>
2022-04-28 19:12:04 +0800

    Create ex20.cpp
    大減小

---
+++ ex20.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int add(int x,int y){
+    return x+y;
+    
+}
+int sub(int x,int y){
+    return (x > y)? x-y:y-x;
+    
+}
+int mul(int x,int y){
+    return x*y;
+}
+int divi(int x,int y){
+    return x/y;
+}
+int main()
+{
+    int x=3,y=6;
+    cout << "add x + y = " << add(x,y) << endl;
+    cout << "sub x - y = " << sub(x,y) << endl;
+    cout << "aws x * y = " << mul(x,y) << endl;
+    cout << "esd x / y = " << divi(x,y) << endl;
+    cout<<"Hello World" << endl;
+    
+return 0;
+
+    
+return 0;
+}
