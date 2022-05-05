acb8d356cc51bc7871d24c52d41eff3e6d939972
chrischu103 <100347499+chrischu103@users.noreply.github.com>
2022-05-05 19:29:37 +0800

    Create ex26.cpp

---
+++ ex26.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+
+    cin >> n;
+
+    i = 1;
+    while( i <= n )
+    {
+        if( n%i == 0 )
+        {
+            cout << i << " ";
+        }
+
+        i = i+1;
+    }
+    cout << endl;
+
+    return 0;
+}
