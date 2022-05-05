2f7a20e694ce39c0e1adac2da3ce7e7200301e7a
chrischu103 <100347499+chrischu103@users.noreply.github.com>
2022-05-05 19:27:26 +0800

    Create ex25.cpp

---
+++ ex25.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int is_prime(int);
+int main()
+{
+	int number;
+	scanf("%d",&number);
+	if(is_prime(number)){
+		printf("是質數\n");
+	}else{
+		printf("不是質數\n");
+	}
+  
+}
+int is_prime(int a){
+	int i;
+	if(a==1){
+		return 0;
+	}else{
+		for(i=2;i<a;i++){
+			if(a%i==0){
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
